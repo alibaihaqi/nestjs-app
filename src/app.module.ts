3ae5c73a998bdbf20789e618d3bba77fbae65e0c
Fadli Al Baihaqi <alibaihaqi1704@gmail.com>
2023-11-16 02:44:21 +0700

    feat: update main and app.module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,20 @@
-import { Module } from '@nestjs/common';
+import { Module, NestModule, MiddlewareConsumer } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 import { CommonModule } from './common/common.module';
 import { MathModule } from './math/math.module';
 import { OpenaiModule } from './openai/openai.module';
+import { LoggerMiddleware } from './middleware/logger.middleware';
 
 @Module({
-  imports: [CommonModule, MathModule, OpenaiModule],
+  imports: [
+    ConfigModule.forRoot({ isGlobal: true }),
+    CommonModule,
+    MathModule,
+    OpenaiModule,
+  ],
 })
-export class AppModule {}
+export class AppModule implements NestModule {
+  configure(consumer: MiddlewareConsumer) {
+    consumer.apply(LoggerMiddleware).forRoutes('common', 'openai/(.*)');
+  }
+}
--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,5 @@
 import { NestFactory } from '@nestjs/core';
+import { ValidationPipe } from '@nestjs/common';
 import {
   FastifyAdapter,
   NestFastifyApplication,
@@ -8,12 +9,21 @@ import { AppModule } from './app.module';
 /**
  * @description: NestJS with Fastify
  */
-async function bootstrapFastify() {
+async function bootstrap() {
   const app = await NestFactory.create<NestFastifyApplication>(
     AppModule,
     new FastifyAdapter({ logger: true }),
   );
+
+  app.useGlobalPipes(
+    new ValidationPipe({
+      // Whitelist is used to filter the request body property that sent to the server but we don't define
+      whitelist: true,
+    }),
+  );
+
   const PORT = process.env.NESTAPP_PORT || 3000;
   await app.listen(PORT, '0.0.0.0');
 }
-bootstrapFastify();
+
+bootstrap();
