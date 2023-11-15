ab405b08bf3b02b2918e33b029bb0364b53ead73
Fadli Al Baihaqi <alibaihaqi1704@gmail.com>
2023-11-16 02:42:44 +0700

    feat: update dto and interfaces

--- src/openai/dto/chat.dto.ts
+++ src/openai/dto/chat.dto.ts
@@ -2,11 +2,11 @@ import { z } from 'zod';
 
 const messageSchema = z.object({
   role: z.enum(['assistant', 'system', 'user']),
-  content: z.string(),
+  content: z.string().nonempty('content cannot be empty'),
 });
 
 export const createChatSchema = z
   .object({
-    messages: z.array(messageSchema),
+    messages: z.array(messageSchema).min(1),
   })
   .required();
--- src/openai/interfaces/chat.ts
+++ src/openai/interfaces/chat.ts
@@ -1,9 +1,15 @@
+import OpenAi from 'openai';
+
 interface IChatMessage {
   role: 'assistant' | 'system' | 'user';
   content: string;
 }
 
+export interface IChatRequest {
+  messages: IChatMessage[];
+}
+
 export interface IChatResponse {
   success: boolean;
-  message: IChatMessage;
+  message: OpenAi.ChatCompletion;
 }
