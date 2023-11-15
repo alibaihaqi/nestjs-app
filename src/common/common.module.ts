5b3485cad3a9b461915e2ff74aa67cbb69a600fd
Fadli Al Baihaqi <alibaihaqi1704@gmail.com>
2023-11-16 02:43:46 +0700

    refactor: common module

--- src/common/common.module.ts
+++ src/common/common.module.ts
@@ -1,14 +1,9 @@
-import { Module, NestModule, MiddlewareConsumer } from '@nestjs/common';
+import { Module } from '@nestjs/common';
 import { CommonController } from './common.controller';
 import { CommonService } from './common.service';
-import { LoggerMiddleware } from '../middleware/logger.middleware';
 
 @Module({
   controllers: [CommonController],
   providers: [CommonService],
 })
-export class CommonModule implements NestModule {
-  configure(consumer: MiddlewareConsumer) {
-    consumer.apply(LoggerMiddleware).forRoutes('common');
-  }
-}
+export class CommonModule {}
