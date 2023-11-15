039300f6855d24862816043858f7c7eaa5e5074f
Fadli Al Baihaqi <alibaihaqi1704@gmail.com>
2023-11-16 00:28:13 +0700

    feat: add method on controller

--- src/openai/openai.controller.ts
+++ src/openai/openai.controller.ts
@@ -1,4 +1,16 @@
-import { Controller } from '@nestjs/common';
-
+import { Controller, HttpCode, Post, UsePipes } from '@nestjs/common';
+import { OpenaiService } from './openai.service';
+import { createChatSchema } from './dto/chat';
+import { IChatResponse } from './interfaces/chat';
+import { ZodValidationPipe } from '../utils/validation-pipes';
 @Controller('openai')
-export class OpenaiController {}
+export class OpenaiController {
+  constructor(private readonly openaiService: OpenaiService) {}
+
+  @Post('/chat')
+  @UsePipes(new ZodValidationPipe(createChatSchema))
+  @HttpCode(200)
+  getChatOpenai(): IChatResponse {
+    return this.openaiService.getChaiOpenaiResponse();
+  }
+}
