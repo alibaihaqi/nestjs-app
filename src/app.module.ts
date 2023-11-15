446144aff2af5936716efae4d1de8cf7c34c502b
Fadli Al Baihaqi <alibaihaqi1704@gmail.com>
2023-11-15 23:51:08 +0700

    feat: add new module openai

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,9 @@
 import { Module } from '@nestjs/common';
 import { CommonModule } from './common/common.module';
 import { MathModule } from './math/math.module';
+import { OpenaiModule } from './openai/openai.module';
 
 @Module({
-  imports: [CommonModule, MathModule],
+  imports: [CommonModule, MathModule, OpenaiModule],
 })
 export class AppModule {}
