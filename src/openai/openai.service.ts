c1b8ead4b8c7fb8393545a2e6c344df2eb343afc
Fadli Al Baihaqi <alibaihaqi1704@gmail.com>
2023-11-16 00:29:32 +0700

    feat: add method on service

--- src/openai/openai.service.ts
+++ src/openai/openai.service.ts
@@ -1,4 +1,15 @@
 import { Injectable } from '@nestjs/common';
+import { IChatResponse } from './interfaces/chat';
 
 @Injectable()
-export class OpenaiService {}
+export class OpenaiService {
+  getChaiOpenaiResponse(): IChatResponse {
+    return {
+      success: true,
+      message: {
+        role: 'assistant',
+        content: 'test',
+      },
+    };
+  }
+}
