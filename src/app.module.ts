65c325fe2905fd7bd89f8a075a68f43f5d87cddb
Fadli Al Baihaqi <alibaihaqi1704@gmail.com>
2023-11-17 23:08:56 +0700

    feat: add aws module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,5 +1,6 @@
 import { Module, NestModule, MiddlewareConsumer } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
+import { AwsModule } from './aws/aws.module';
 import { CommonModule } from './common/common.module';
 import { MathModule } from './math/math.module';
 import { OpenaiModule } from './openai/openai.module';
@@ -8,6 +9,7 @@ import { LoggerMiddleware } from './middleware/logger.middleware';
 @Module({
   imports: [
     ConfigModule.forRoot({ isGlobal: true }),
+    AwsModule,
     CommonModule,
     MathModule,
     OpenaiModule,
