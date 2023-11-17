9b4410da2df425943df913a454a5b7579718e205
Fadli Al Baihaqi <alibaihaqi1704@gmail.com>
2023-11-17 23:09:18 +0700

    feat: add audio interface on openai

---
+++ src/openai/interfaces/audio.ts
@@ -0,0 +1,16 @@
+export interface IAudioMessageRequest {
+  input: string;
+  voice: 'alloy' | 'echo' | 'fable' | 'onyx' | 'nova' | 'shimmer';
+  response_format?: 'mp3' | 'opus' | 'aac' | 'flac';
+}
+
+export interface IAudioOpenaiRequest extends IAudioMessageRequest {
+  model: string;
+}
+
+export interface IAudioMessageResponse {
+  success: boolean;
+  errorMessage?: string;
+  name?: string;
+  data?: any;
+}
