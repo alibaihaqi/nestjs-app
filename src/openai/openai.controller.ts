fce9a84ed7dcd1d149013526a7904a9ecc1f6dc7
Fadli Al Baihaqi <alibaihaqi1704@gmail.com>
2023-11-17 23:09:47 +0700

    feat: add text to audio service on openai controller and service

--- src/openai/openai.controller.ts
+++ src/openai/openai.controller.ts
@@ -1,12 +1,21 @@
 import { Body, Controller, HttpCode, Post, UsePipes } from '@nestjs/common';
 import { OpenaiService } from './openai.service';
-import { createChatSchema } from './dto/chat.dto';
-import { IChatRequest, IChatResponse } from './interfaces/chat';
+import { createChatSchema, audioRequestSchema } from './dto';
+import {
+  IAudioMessageRequest,
+  IChatRequest,
+  IChatResponse,
+} from './interfaces';
+import { IS3Response } from '../aws/interfaces';
+import { AwsService } from '../aws/aws.service';
 import { ZodValidationPipe } from '../utils/validation-pipes';
 
 @Controller('openai')
 export class OpenaiController {
-  constructor(private readonly openaiService: OpenaiService) {}
+  constructor(
+    private readonly openaiService: OpenaiService,
+    private readonly awsService: AwsService,
+  ) {}
 
   @Post('/chat')
   @UsePipes(new ZodValidationPipe(createChatSchema))
@@ -14,5 +23,39 @@ export class OpenaiController {
   async getChatOpenai(@Body() request: IChatRequest): Promise<IChatResponse> {
     const getMessages = await this.openaiService.getMessagesData(request);
     return this.openaiService.getChatOpenaiResponse(getMessages);
+  }
+
+  @Post('/text-to-audio')
+  @UsePipes(new ZodValidationPipe(audioRequestSchema))
+  @HttpCode(200)
+  async getTextToAudio(
+    @Body() request: IAudioMessageRequest,
+  ): Promise<IS3Response> {
+    try {
+      const speechData = await this.openaiService.getAudioSpeechData(request);
+
+      if (!speechData.success) {
+        throw {
+          success: speechData.success,
+          errorMessage: speechData.errorMessage,
+        };
+      }
+
+      const awsResponse = await this.awsService.uploadDataToAwsS3Bucket({
+        name: speechData.name,
+        data: speechData.data,
+      });
+
+      if (!awsResponse.success) {
+        throw {
+          success: awsResponse.success,
+          errorMessage: awsResponse.errorMessage,
+        };
+      }
+
+      return awsResponse;
+    } catch (error) {
+      return error;
+    }
   }
 }
--- src/openai/openai.service.ts
+++ src/openai/openai.service.ts
@@ -1,17 +1,26 @@
 import { Injectable } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
 import OpenAI from 'openai';
-import { IChatRequest, IChatResponse } from './interfaces/chat';
+import { APIPromise } from 'openai/core';
+import {
+  IAudioMessageRequest,
+  IAudioMessageResponse,
+  IChatRequest,
+  IChatResponse,
+} from './interfaces';
+import { genUlid } from '../utils/ulid';
 
 @Injectable()
 export class OpenaiService {
   private openai: OpenAI;
 
   constructor(private configService: ConfigService) {
-    this.openai = new OpenAI();
+    this.openai = new OpenAI({
+      apiKey: this.configService.get('OPENAI_API_KEY'),
+    });
   }
 
-  getMessagesData(request: IChatRequest) {
+  getMessagesData(request: IChatRequest): APIPromise<OpenAI.ChatCompletion> {
     return this.openai.chat.completions.create({
       model: this.configService.get('OPENAI_API_MODEL'),
       messages: request.messages,
@@ -23,5 +32,37 @@ export class OpenaiService {
       success: true,
       message: message,
     };
+  }
+
+  async getAudioSpeechData(
+    request: IAudioMessageRequest,
+  ): Promise<IAudioMessageResponse> {
+    const response_format =
+      request.response_format ||
+      this.configService.get('OPENAI_API_AUDIO_OUTPUT');
+
+    const audioRequest: OpenAI.Audio.Speech.SpeechCreateParams = {
+      ...request,
+      model: this.configService.get('OPENAI_API_AUDIO_MODEL'),
+      response_format,
+    };
+
+    try {
+      const audioResponse = await this.openai.audio.speech.create(audioRequest);
+      const buffer = Buffer.from(await audioResponse.arrayBuffer());
+
+      return {
+        success: true,
+        data: buffer,
+        name: `${this.configService.get(
+          'AWS_OPENAI_PATH',
+        )}/audio/audio-${genUlid}.${response_format}`,
+      };
+    } catch (error) {
+      return {
+        success: false,
+        errorMessage: error,
+      };
+    }
   }
 }
