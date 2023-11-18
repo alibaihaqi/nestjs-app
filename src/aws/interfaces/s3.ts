8f14da43835d412556ebf85aaaff6c1d69b3c1fd
Fadli Al Baihaqi <alibaihaqi1704@gmail.com>
2023-11-18 21:45:16 +0700

    feat: add new interface to s3

--- src/aws/interfaces/s3.ts
+++ src/aws/interfaces/s3.ts
@@ -1,10 +1,15 @@
-export interface IS3Request {
+export interface IS3UploadRequest {
   name: string;
   data: any;
 }
 
-export interface IS3Response {
+export interface IS3UploadResponse {
   success: boolean;
   errorMessage?: string;
   assetPath?: string;
+}
+
+export interface IS3GetAssetRequest {
+  assetPath: string;
+  bucketName?: string;
 }
