b64c4ee4297fe73d921d72f5212ecc96c5ecf455
Fadli Al Baihaqi <alibaihaqi1704@gmail.com>
2024-01-22 10:07:13 +0700

    feat: add handleRtcConnection rtc service

--- src/rtc/rtc.controller.ts
+++ src/rtc/rtc.controller.ts
@@ -1,4 +1,18 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Post, UsePipes } from '@nestjs/common';
+
+import { RtcService } from './rtc.service';
+import { connectClientSchema } from './dto';
+
+import { ZodValidationPipe } from '../utils/validation-pipes';
+import { IRtcConnectClient } from './interfaces';
 
 @Controller('rtc')
-export class RtcController {}
+export class RtcController {
+  constructor(private readonly rtcService: RtcService) {}
+
+  @Post('/connect')
+  @UsePipes(new ZodValidationPipe(connectClientSchema))
+  async handleRtcConnection(@Body() request: IRtcConnectClient): Promise<any> {
+    return this.rtcService.addClientConnectionId(request);
+  }
+}
