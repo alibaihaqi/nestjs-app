95ed25859fa739fef1153177d2d858eb6fcfef61
Fadli Al Baihaqi <alibaihaqi1704@gmail.com>
2024-01-22 10:06:57 +0700

    feat: add addClientConnectionId rtc service

--- src/rtc/rtc.service.ts
+++ src/rtc/rtc.service.ts
@@ -1,4 +1,20 @@
 import { Injectable } from '@nestjs/common';
+import { IRtcConnectClient } from './interfaces';
+import { PrismaService } from '../prisma/prisma.service';
 
 @Injectable()
-export class RtcService {}
+export class RtcService {
+  constructor(private readonly prismaService: PrismaService) {}
+
+  async addClientConnectionId(request: IRtcConnectClient) {
+    return this.prismaService['rtc_socket_user'].create({
+      data: {
+        connectionId: request.connectionId,
+      },
+      select: {
+        connectionId: true,
+        userId: true,
+      },
+    });
+  }
+}
